1200f2d5e2253e3ee38ea58cbd8e2f444d730676
ImAmlan03 <imamlan03@gmail.com>
2024-08-13 12:25:24 +0530

    created model, controller and route for category

--- database/migrations/2024_08_13_063412_create_category_table.php
+++ database/migrations/2024_08_13_063412_create_category_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('category', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('is_active')->default(1);
             $table->timestamps();
         });
     }
